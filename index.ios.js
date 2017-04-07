ccc2d785a1d66cec5dd3bee2a71bc26d97157761
mjdude <mo@syncspot.net>
2017-04-07 16:25:16 -0700

    added header component

--- index.ios.js
+++ index.ios.js
@@ -1,12 +1,13 @@
 // Index.ios.js place code in here for ios
 import React from 'react';
 import {Text, AppRegistry} from 'react-native';
+import Header from './src/components/header';
 
 // create the component
 
 const App = () => {
   return (
-    <Text>Some Text</Text>
+    <Header/>
   )
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+//import lib for making a component
+import React from 'react';
+import {Text} from 'react-native';
+
+
+// make component
+
+const Header = () => {
+    return <Text>Albums</Text>;
+}
+
+// make components avaliable to other parts of the app
+export default Header;

f2a81db1ec5d094d189bc1755b56e66d247a9171
mjdude <mo@syncspot.net>
2017-04-07 16:49:12 -0700

    added header styles

--- src/components/header.js
+++ src/components/header.js
@@ -20,7 +20,14 @@ const styles = {
     viewStyle: {
         backgroundColor: '#F8F8F8',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        height: 60,
+        padding: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2},
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
     },
 
     textStyle: {

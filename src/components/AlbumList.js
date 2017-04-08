193c52ecdad4e788435119fcb140623fb6383b3d
mjdude <mo@syncspot.net>
2017-04-07 17:49:36 -0700

    added api fetch

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,6 +4,14 @@ import {Text, View} from 'react-native';
 
 
 class AlbumList extends Component {
+    componentWillMount(){
+        fetch('https://rallycoding.herokuapp.com/api/music_albums')
+            .then((res) => {
+                return res.json()
+            }).then((resData) => {
+                console.log(resData);
+            })
+    }
     render(){
         return (
             <View>

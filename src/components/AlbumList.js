098a879e6e964affcde3775c4f25a0baf7963a0c
mjdude <mo@syncspot.net>
2017-04-07 17:13:01 -0700

    changed AlbumList to class based component form functional component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,14 +1,16 @@
 //import lib for making a component
-import React from 'react';
+import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>Album List</Text>
-        </View>
-    )
+class AlbumList extends Component {
+    render(){
+        return (
+            <View>
+                <Text>Album List</Text>
+            </View>
+        )
+    }   
 }
 
 export default AlbumList;

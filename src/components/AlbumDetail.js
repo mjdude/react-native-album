2c2addbdd3473852fc4218b5d202fbad888b2e58
mjdude <mo@syncspot.net>
2017-04-07 20:09:14 -0700

    added imagestyle

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -6,21 +6,26 @@ import CardSection from './CardSection';
 // {album} is destructuring album from props.album
 // image will need a style with width and height in order to render a viewable size
 const AlbumDetail = ({album}) => {
-    const {artist, title, thumbnail_image} = album;
-    const {thumbnailStyle, headerContentStyle} = styles;
+    const {artist, title, thumbnail_image, image} = album;
+    const {thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle} = styles;
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image 
                     style={thumbnailStyle}
                     source={{uri: thumbnail_image}}></Image>
                 </View>
                 <View style={headerContentStyle}>
-                    <Text>{title}</Text>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>   
                 </View>             
-            </CardSection>        
+            </CardSection>  
+            <CardSection>
+                <Image 
+                style={imageStyle}
+                source={{uri: image}}></Image>
+            </CardSection>      
         </Card>
     );
 };
@@ -30,9 +35,26 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around',
     },
+    headerTextStyle: {
+        fontSize: 18
+    },
+
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+
+    },
+
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 }
 

1b857b3970e5c393dc2a89dc5c361e0fbe48e902
mjdude <mo@syncspot.net>
2017-04-07 18:38:27 -0700

    added card component

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+
+const Card = (props) => {
+    return (
+        <View>
+        </View>
+    );
+};
+
+export default Card;

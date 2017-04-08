3531dcd5fced9b1af44c9dac2f126ed5d0632154
mjdude <mo@syncspot.net>
2017-04-07 18:05:15 -0700

    added state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,15 +4,18 @@ import {Text, View} from 'react-native';
 
 
 class AlbumList extends Component {
+    state = {albums: []};
+
     componentWillMount(){
         fetch('https://rallycoding.herokuapp.com/api/music_albums')
             .then((res) => {
                 return res.json()
             }).then((resData) => {
-                console.log(resData);
+                this.setState({albums: resData})
             })
     }
     render(){
+        console.log(this.state);
         return (
             <View>
                 <Text>Album List</Text>
